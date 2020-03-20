d47b443bfcda4506255c29b873f166511b28bf1f
rabah babaci <4rabah@gmail.com>
2020-03-20 13:47:20 -0700

    add test for input fields while a user is typing

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -16,10 +16,17 @@ test('form submit renders a JSON object', () => {
   const { getByTestId } = render(<ContactForm />);
 
   const firstName = getByTestId('first-name-input');
-  //   const lastName = getByTestId('last name');
-  //   const email = getByTestId('email');
-  //   const message = getByTestId('message');
+  const lastName = getByTestId('last-name-input');
+  const email = getByTestId('email-input');
+  const message = getByTestId('message-input');
 
   fireEvent.change(firstName, { target: { value: 'test first name' } });
+  fireEvent.change(lastName, { target: { value: 'test last name' } });
+  fireEvent.change(email, { target: { value: 'test email' } });
+  fireEvent.change(message, { target: { value: 'test message' } });
+
   expect(firstName.value).toBe('test first name');
+  expect(lastName.value).toBe('test last name');
+  expect(email.value).toBe('test email');
+  expect(message.value).toBe('test message');
 });
